9454ee5d9300643f8f4e19f6ce91cc3075f0efa9
Venryx <venryx@gmail.com>
2016-12-28 07:28:12 -0800

    *

--- Main/Source/LucidLink/More/AboutUI.tsx
+++ Main/Source/LucidLink/More/AboutUI.tsx
@@ -1,12 +1,13 @@
 import {Text} from "react-native";
-import {BaseComponent, Panel} from "../../Frame/ReactGlobals";
+import {BaseComponent as Component, Panel} from "../../Frame/ReactGlobals";
 import {colors, styles} from "../../Frame/Styles";
-export default class AboutUI extends BaseComponent<any, any> {
+
+export default class AboutUI extends Component<any, any> {
 	render() {
 		return (
 			<Panel style={{flex: 1, flexDirection: "column", backgroundColor: colors.background}}>
 				<Text style={styles.text}>{`
-"Monitors Muse headband EEG data, and triggers events (eg audio) when REM sleep is detected."
+"Monitors Muse headband EEG data, and triggers user-scripted events (eg audio) when REM sleep is detected."
 
 Author: Stephen Wicklund (Venryx)
 License: MIT (open source)
