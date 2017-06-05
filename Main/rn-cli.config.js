1722b012aa1dc27cc87206379cfcd4a85f97f302
Venryx <venryx@gmail.com>
2017-06-05 10:15:26 -0700

    * Found that packager error still shows up sometimes. ; (   [for now, seems to work fine if you start packager after run-android build-process]
    Next:
    * Fix that streaming of real-time data slows the UI down too much.

--- Main/rn-cli.config.js
+++ Main/rn-cli.config.js
@@ -1,20 +1,16 @@
 let blacklist = require('react-native/packager/blacklist');
 let config = {
-    getBlacklistRE() {
+	getBlacklistRE() {
 		console.log("Getting blacklist...");
-        return blacklist([
-            // Ignore local `.sample.js` files.
-            /.*\.sample\.js$/,
-
-            // Ignore IntelliJ directories
-            /.*\.idea\/.*/,
-            // ignore git directories
-            /.*\.git\/.*/,
-            // Ignore android directories
-            /.*\/app\/build\/.*/,
-
-            // Add more regexes here for paths which should be blacklisted from the packager.
-        ]);
-    }
+		return blacklist([
+			// Ignore IntelliJ directories
+			// /.*\.idea\/.*/,
+			// ignore git directories
+			// /.*\.git\/.*/,
+			// Ignore android directories
+			// /.*\/app\/build\/.*/,
+			/.*\/app\/build\/.*/,
+		]);
+	}
 };
 module.exports = config;
